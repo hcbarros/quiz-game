7b56d53f8d109b0415e510bee3273c4f4814efd3
hcbarros <hcb@a.recife.ifpe.edu.br>
2021-02-28 00:48:04 -0300

    first commit

---
+++ src/main/java/br/com/quizGame/QuizGameApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.quizGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGameApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/quizGame/QuizGameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quizGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
